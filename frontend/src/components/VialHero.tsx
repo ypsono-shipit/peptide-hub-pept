44478dc19ba72ab0d8ca703a0b640ed8d9937fc7
ypsono <xaitoshi@gmail.com>
2026-07-15 18:39:43 +0800

    Show full PEPT vial image edge-to-edge without black panel fill.

--- frontend/src/components/VialHero.tsx
+++ frontend/src/components/VialHero.tsx
@@ -3,12 +3,12 @@
 export function VialHero({ symbol }: { symbol: string }) {
   return (
     <div className="relative flex min-h-[240px] flex-col overflow-hidden rounded-xl border border-border bg-panel">
-      <div className="relative z-10 flex flex-1 items-center justify-center bg-black p-3">
+      <div className="relative z-10 flex flex-1 items-center justify-center p-0">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
           src="/pept-vial.jpg"
           alt={`${symbol} research vial`}
-          className="max-h-[240px] w-auto max-w-full object-contain"
+          className="h-full min-h-[240px] w-full object-cover object-center"
         />
       </div>
       <div className="relative z-10 border-t border-border px-3 py-2 text-center">
